cbdc1968ba8dfc632d27a33a2dc242f32f102baa
hyemi-bang <pbhm1124@gmail.com>
2024-03-25 19:41:58 +0900

    update

--- repos/MyFirstConsoleApp/Program.cs
+++ repos/MyFirstConsoleApp/Program.cs
@@ -8,7 +8,28 @@ namespace MyFirstConsoleApp
     {//클래스는 프로그램의 일부분, 메서드를 1개이상 포함
         static void Main(string[] args)
         {// 메서드는 클래스에 속해있으며 보통 Console.WriteLine 같은 명령문 여러개로 이루어짐
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            OperatorExamples();// Compiler Error CS0103
+        }
+
+        private static void OperatorExamples()
+        {
+            //throw new NotImplementedException(); 
+            //vs에서 자동으로 메서드 생성할때 자리표시자(placeholder)에 throw 명령어 생성
+
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "The area ";
+            result = result + "is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
         }
     }
 }
