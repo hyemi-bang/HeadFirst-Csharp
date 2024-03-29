406595149eef7ce238b95b2e2b5c12ea31cf44c3
hyemi-bang <pbhm1124@gmail.com>
2024-03-29 15:45:16 +0900

    CardPicker game

--- repos/PickRandomCards/Program.cs
+++ repos/PickRandomCards/Program.cs
@@ -6,7 +6,7 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the number of cards to pick :");
+            Console.Write("Enter the number of cards to pick : ");
 
             string line = Console.ReadLine();
             if (int.TryParse(line, out int numberOfCards))
